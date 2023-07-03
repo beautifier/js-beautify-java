b9b7c4bb3e32625da57f55b8ef01d6c7b9a0ab87
Karl von Randow <karl@xk72.com>
2023-07-03 21:26:13 +1200

    test: improve OptionsTests

--- src/test/java/io/beautifier/css/OptionsTests.java
+++ src/test/java/io/beautifier/css/OptionsTests.java
@@ -1,16 +1,44 @@
 package io.beautifier.css;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+
+import io.beautifier.css.CSSOptions.BraceStyle;
 
 public class OptionsTests {
 	
 	@Test
-	void constructOptions() {
-		CSSOptions.builder()
+	void simpleOptions() {
+		CSSOptions options = CSSOptions.builder()
 			.disabled(false)
 			.indent_size(4)
 			.newline_between_rules(true)
+			.build();
+		assertFalse(options.disabled);
+		assertEquals(4, options.indent_size);
+		assertTrue(options.newline_between_rules);
+		assertEquals(BraceStyle.collapse, options.brace_style); /* The default */
+	}
+
+	@Test
+	void cssSpecificOptions() {
+		CSSOptions.Builder builder = CSSOptions.builder()
+			.indent_size(5)
 			;
+		builder.css()
+			.indent_size(6)
+			;
+		builder.js()
+			.indent_size(7)
+			;
+		builder.html()
+			.indent_size(8)
+			;
+		CSSOptions options = builder.build();
+		assertEquals(6, options.indent_size);
 	}
 
 }
